c28c5c5467cabb5937c063cb0962865320649c0f
Your Name <you@example.com>
2024-01-03 17:46:05 +0100

    fixed undefined video results array empty

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -8,7 +8,10 @@ import Image from 'react-bootstrap/Image'
 const Movie = ({ movie }) => {
 
     const img = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-    const video = `https://www.youtube.com/embed/${movie.videos.results[0].key}`
+    
+    let key = (movie.videos.results.length !== 0) ? movie.videos.results[0].key : null
+
+    const video = `https://www.youtube.com/embed/${key}`
 
     return (
         <Container>
@@ -21,7 +24,7 @@ const Movie = ({ movie }) => {
                 <Col>
                     <Row>{movie.overview}</Row>
                     <br/>
-                    <Row><Container><iframe width="420" height="315" src={video} /></Container></Row>  
+                    <Row>{(key) ? <Container><iframe width="420" height="315" src={video} /></Container> : <></>}</Row>  
                 </Col>
             </Row>
 
