4bd99b1ceb5088ea806a8da6f5b1a6cc6d251c9b
Your Name <you@example.com>
2024-01-03 16:31:09 +0100

    added video embeded

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -8,14 +8,21 @@ import Image from 'react-bootstrap/Image'
 const Movie = ({ movie }) => {
 
     const img = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+    const video = `https://www.youtube.com/embed/${movie.videos.results[0].key}`
 
     return (
         <Container>
-            <h3 className='text-info text-center'>{movie.original_title}</h3>
+            <Row>
+              <Col />
+              <Col><h3 className='text-info text-center'>{movie.original_title}</h3></Col>  
+            </Row>
             <Row>
                 <Col> <Image src={img} thumbnail /></Col>
-                <Col>{movie.overview}</Col>
+                <Col>{movie.overview}
+                    <Row><br/><Container><iframe width="420" height="315" src={video} /></Container></Row>  
+                </Col>
             </Row>
+
         </Container>
     ) 
 
