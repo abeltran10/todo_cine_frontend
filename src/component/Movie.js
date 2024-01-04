3067894b9d66cc198f3902ed1cb830959d0ab9fe
Your Name <you@example.com>
2024-01-04 16:45:11 +0100

    display movie genres

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -11,18 +11,35 @@ const Movie = ({ movie }) => {
     
     const video = (movie.videos.results.length !== 0) ? `https://www.youtube.com/embed/${movie.videos.results[0].key}` : null
 
+    const releaseDate = `(${movie.release_date.substring(0, movie.release_date.indexOf("-"))})`
+
+
+    let genresName = ''
+
+    const genres = movie.genres
+
+    for (let i = 0; i < genres.length ; i++) {
+        if (i + 1 === genres.length)
+            genresName = genresName.concat(genres[i].name)
+        else
+            genresName = genresName.concat(`${genres[i].name} | `)
+    }
+
     return (
         <Container>
             <Row>
               <Col />
-              <Col><h3 className='text-info text-center'>{movie.original_title}</h3></Col>  
+              <Col><h3 className='text-info text-center'>{movie.title}  {releaseDate}</h3></Col>  
             </Row>
             <Row>
-                <Col> <Image src={img} thumbnail /></Col>
+                <Col><Image src={img} thumbnail /></Col>
                 <Col>
                     <Row>{movie.overview}</Row>
                     <br/>
-                    <Row>{(video) ? <Container><iframe width="420" height="315" src={video} /></Container> : <></>}</Row>  
+                    <Row>{(video) ? <Container><iframe width="420" height="315" src={video} /></Container> : <></>}</Row> 
+                    <Row />
+                    <br/>
+                    <Row><Container>Generos: {genresName}</Container></Row>
                 </Col>
             </Row>
 
--- src/component/NavigationBar.js
+++ src/component/NavigationBar.js
@@ -1,5 +1,6 @@
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import Button from 'react-bootstrap/Button'
 
 const NavigationBar = ({username, logout}) => {
@@ -11,20 +12,21 @@ const NavigationBar = ({username, logout}) => {
     await logout()
   } 
 
-
+  const signed = `Signed as ${username}`
+  
     return (
-        <Navbar className="bg-body-tertiary">
-          <Container>
-            <Navbar.Brand href="#home">{username}</Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-              <Navbar.Text>
-                <Button variant="link" onClick={handleLogout}>Logout</Button>
-              </Navbar.Text>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      )
+       <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">Todo Cine</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+          <NavDropdown title={signed} id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#logout"><Button variant="danger" onClick={handleLogout}>Log out</Button></NavDropdown.Item>
+            </NavDropdown>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
 }
 
 export default NavigationBar
